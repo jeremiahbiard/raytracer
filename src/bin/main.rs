dc49e56c96e919ec423f7e1dd3656f844272a073
Jeremiah Biard <jeremiahbiard@outlook.com>
2023-02-19 09:16:20 -0500

    Write image to png.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use png::{ColorType, Encoder};
+// use png; //::{ColorType, Encoder};
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
@@ -6,12 +6,15 @@ use std::path::Path;
 fn main() {
     // Image
 
-    const IMG_HEIGHT: u32 = 256;
-    const IMG_WIDTH: u32 = 256;
+    const IMG_HEIGHT: u32 = 480;
+    const IMG_WIDTH: u32 = 640;
     const MN: f32 = 255.999;
+    const SIZE: usize = (IMG_HEIGHT * IMG_WIDTH * 3) as usize;
+
+    // Create png file
     let path = Path::new(r"image.png");
     let file = File::create(path).unwrap();
-    let ref mut w = BufWriter::new(file);
+    let w = &mut BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, IMG_WIDTH, IMG_HEIGHT);
 
@@ -21,29 +24,25 @@ fn main() {
     let mut writer = encoder.write_header().unwrap();
 
     // Render
-    print!("P3\n{} {}\n255\n", IMG_WIDTH, IMG_HEIGHT);
 
-    let mut data: Vec<u8> = Vec::new();
-    for j in (0..IMG_HEIGHT - 1).rev() {
+    let mut data: [u8; SIZE] = [0; SIZE];
+    let mut index: usize = 0;
+    for j in (0..IMG_HEIGHT).rev() {
+        println!("{} scanlines left...", j);
         for i in 0..IMG_WIDTH {
             let r = i as f32 / (IMG_WIDTH - 1) as f32;
             let g = j as f32 / (IMG_HEIGHT - 1) as f32;
             let b = 0.25;
 
-            let ir = (MN * r) as u8;
-            let ig = (MN * g) as u8;
-            let ib = (MN * b) as u8;
-
-            data.push(ir);
-            data.push(ig);
-            data.push(ib);
-            // println!("{} {} {}", ir, ig, ib);
+            data[index] = (MN * r) as u8;
+            index += 1;
+            data[index] = (MN * g) as u8;
+            index += 1;
+            data[index] = (MN * b) as u8;
+            index += 1;
         }
     }
-    let array: [u8; (IMG_HEIGHT * IMG_WIDTH) as usize] =
-        match data.try_into().unwrap_or_else(|error| {
-            panic!("Problem: {:?}", error);
-        }) {};
+    println!("Finished!");
 
-    writer.write_image_data(&array).unwrap();
+    writer.write_image_data(&data).unwrap();
 }
