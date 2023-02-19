db75905e7c8eea2f00a6c3449802336d59415f2d
Jeremiah Biard <jeremiahbiard@outlook.com>
2023-02-18 21:32:00 -0500

    Render first image to .ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello Raytracer!");
+    // Image
+
+    const IMG_HEIGHT: i32 = 256;
+    const IMG_WIDTH: i32 = 256;
+    const MN: f32 = 255.999;
+    // Render
+    print!("P3\n{} {}\n255\n", IMG_WIDTH, IMG_HEIGHT);
+
+    for j in (0..IMG_HEIGHT - 1).rev() {
+        for i in 0..IMG_WIDTH {
+            let r = i as f32 / (IMG_WIDTH - 1) as f32;
+            let g = j as f32 / (IMG_HEIGHT - 1) as f32;
+            let b = 0.25;
+
+            let ir = (MN * r) as i32;
+            let ig = (MN * g) as i32;
+            let ib = (MN * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
